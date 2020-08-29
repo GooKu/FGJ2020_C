1f88c76fe7e1b899eaef20f36e8f2486a668c3b9
Goo <kuwoods@hotmail.com>
2020-08-29 20:33:18 +0800

    Implement AutoKill and setup at explosion.

---
+++ Assets/Scripts/AutoKill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoKill : MonoBehaviour
+{
+    [SerializeField]
+    private float existTime = .5f;
+
+    private void Update()
+    {
+        existTime -= Time.deltaTime;
+
+        if(existTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
