e0943e3d4f33947c84394d62bd1661a3ff8b5870
Goo <kuwoods@hotmail.com>
2020-08-29 23:35:02 +0800

    Implement GameUIManager.

---
+++ Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameOverUI = null;
+    [SerializeField]
+    private GameObject gameEndUI = null;
+
+    private void Start()
+    {
+        EventManager.AddListen(GameEvents.GameOver, GameOverHandle);
+        EventManager.AddListen(GameEvents.GameEnd, GameEndHandle);
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.RemoveListen(GameEvents.GameOver, GameOverHandle);
+        EventManager.RemoveListen(GameEvents.GameEnd, GameEndHandle);
+    }
+
+    private void GameOverHandle(object[] callBack)
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void GameEndHandle(object[] callBack)
+    {
+        gameEndUI.SetActive(true);
+    }
+}
