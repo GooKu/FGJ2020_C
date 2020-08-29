5d2ab1036b26f7a46bf19d4643a42c56b4ce90b1
Goo <kuwoods@hotmail.com>
2020-08-29 13:41:53 +0800

    Implement bear move control.

---
+++ Assets/Scripts/BearController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5;
+
+    private Vector3 moveVect = Vector3.zero;
+
+    public void Move(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                moveVect = Vector3.up;
+                break;
+            case Direction.Down:
+                moveVect = Vector3.down;
+                break;
+            case Direction.Left:
+                moveVect = Vector3.left;
+                break;
+            case Direction.Right:
+                moveVect = Vector3.right;
+                break;
+        }
+    }
+
+    public void Stop()
+    {
+        moveVect = Vector3.zero;
+    }
+
+    public void Stealth()
+    {
+
+    }
+
+    public void Pick()
+    {
+
+    }
+
+    public void PutDown()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += moveVect * moveSpeed * Time.fixedDeltaTime;
+    }
+}
