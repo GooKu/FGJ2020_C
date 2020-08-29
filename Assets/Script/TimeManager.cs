71fd2f225ef87b344fca322902d9d6c875031849
HashBrown0707 <1qaz987@gmail.com>
2020-08-29 19:21:24 +0800

    timer

---
+++ Assets/Script/TimeManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private Text timer;
+    [SerializeField] private int totalGameTime;
+    private float time;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timer.text = $"Time: {totalGameTime - (int)time}";
+    }
+}
