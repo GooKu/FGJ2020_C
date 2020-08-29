48b640d25988f3dedf1abf55bcf5928b9f0df43f
HashBrown0707 <1qaz987@gmail.com>
2020-08-29 16:48:28 +0800

    patrol

---
+++ Assets/Script/People/Hunter.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : People
+{
+    [SerializeField] private float shootRadius;
+
+    [SerializeField] private Transform[] wayPoints = new Transform[10];
+    private Transform lastWayPoint;
+
+    [SerializeField] private float wayPointDetectDist;
+    private int randomIndex = -1;
+
+    protected override void DetectPlayer(float detectRadius)
+    {
+        base.DetectPlayer(detectRadius);
+
+        if (player != null && player.gameObject.CompareTag("Player"))
+        {
+            //chase && shoot target
+            
+            ChaseTarget(player.transform, moveSpeed);
+
+
+        }
+        else
+        {
+            //patrol
+            Patrol();
+        }
+    }
+
+    private void ChaseTarget(Transform target, float moveSpeed)
+    {
+        
+        float dist = (transform.position - target.position).sqrMagnitude;
+
+        if (dist <= shootRadius * shootRadius)
+        {
+            Shoot();
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);   //移到player
+        }
+    }
+
+    private void Patrol()
+    {
+        //random choose next point to go
+
+        if(randomIndex < 0)
+        {
+            randomIndex = Random.Range(0, wayPoints.Length);
+        }
+        
+        if ((transform.position - wayPoints[randomIndex].position).sqrMagnitude > wayPointDetectDist * wayPointDetectDist) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, wayPoints[randomIndex].position, moveSpeed * Time.deltaTime);    //移到點
+        }
+        else    //到點
+        {
+            randomIndex = -1;
+        }
+
+    }
+
+    private void Shoot()
+    {
+        //die
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, detectRadius);
+    }
+}
---
+++ Assets/Script/People/People.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class People : MonoBehaviour
+{
+    [SerializeField] protected LayerMask playerLayer;
+    protected Collider2D player;
+
+    [SerializeField] protected float detectRadius;
+    [SerializeField] protected float moveSpeed;
+
+    public void Update()
+    {
+        DetectPlayer(detectRadius);
+    }
+
+    protected virtual void DetectPlayer(float detectRadius)
+    {
+        player = Physics2D.OverlapCircle(transform.position, detectRadius, playerLayer);
+
+        
+    }
+
+
+}
