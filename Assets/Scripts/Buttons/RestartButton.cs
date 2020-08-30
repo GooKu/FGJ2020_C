f9e516c9e1e0fab6b90f0fc15b8b159898e7d687
Goo <kuwoods@hotmail.com>
2020-08-30 10:08:57 +0800

    Implement restart and return menu button, and import into UI.

---
+++ Assets/Scripts/Buttons/RestartButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class RestartButton : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Restart);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
