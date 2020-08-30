816d0a17011d7689d25c66c8200bc1b5785cb937
Goo <kuwoods@hotmail.com>
2020-08-30 09:38:46 +0800

    Implement game end at TimeManager, and setup at Game.unity.

--- Assets/Script/TimeManager.cs
+++ Assets/Script/TimeManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
@@ -9,9 +7,30 @@ public class TimeManager : MonoBehaviour
     [SerializeField] private int totalGameTime;
     private float time;
 
+    private void Start()
+    {
+        EventManager.AddListen(GameEvents.GameOver, Stop);
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.RemoveListen(GameEvents.GameOver, Stop);
+    }
+
     private void Update()
     {
         time += Time.deltaTime;
         timer.text = $"Time: {totalGameTime - (int)time}";
+
+        if(time >= totalGameTime)
+        {
+            EventManager.SendEvent(GameEvents.GameEnd);
+            enabled = false;
+        }
+    }
+
+    private void Stop(object[] callBack)
+    {
+        enabled = false;
     }
 }
